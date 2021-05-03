ac3402dcc602e25e3430c699203fb5dccde00a42
Erik Rose <erik@mozilla.com>
2021-05-03 12:50:01 -0400

    Plot just the first 10 clusters, because after that we run out of colors.
    Print averages distances from centers of each cluster, too. Trying to come up with a number that correlates with quality. This one might, but it's not sure.

--- tab_clusters.py
+++ tab_clusters.py
@@ -10,6 +10,7 @@ from numpy import array
 from pandas import DataFrame
 from pyquery import PyQuery as pq
 import seaborn
+from scipy.linalg import norm
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -77,8 +78,8 @@ def tab_clusters(folder, lsa):
 
     # Viz:
     frames = []
-    for cluster_num, cluster in enumerate(vector_clusters):
-        lower_dimensional = TSNE().fit_transform(vector_clusters[0])  # Should I fit this once and then use it to transform them all?
+    for cluster_num, cluster in enumerate(vector_clusters[:9]):
+        lower_dimensional = TSNE(perplexity=100).fit_transform(vector_clusters[0])  # Should I fit this once and then use it to transform them all?
         for x, y in lower_dimensional:
             frames.append((x, y, cluster_num))
     seaborn.scatterplot(
@@ -92,7 +93,21 @@ def tab_clusters(folder, lsa):
 
     for path_index, label in enumerate(clustering.labels_):
         path_clusters[label].append(paths[path_index])
-    pprint(path_clusters)
+    spreads_and_paths = [(spread(vector_clusters[i]), pc) for i, pc in enumerate(path_clusters)]
+    spreads_and_paths.sort()
+    pprint(spreads_and_paths)
+
+
+def spread(vector_cluster):
+    """Return the average distance from the center of the cluster to each node."""
+    def distance(a, b):
+        """Take difference between 2 vectors, then report distance from origin.
+        This is the same as the distance between the 2 vectors."""
+        return norm(a - b)
+
+    length = len(vector_cluster)
+    center = sum(vector_cluster) / length
+    return sum(distance(vector, center) for vector in vector_cluster) / length
 
 
 def text_from_sample(filename):
@@ -107,3 +122,4 @@ if __name__ == '__main__':
 
 # NEXT: HTML signal, throw the URL or domain in as signal. Measure that first cluster, which seems to be pretty misc, so see if it has high spread/variance or something that I can ignore it based on.
 # TODO: Try switching to cosine distance rather than Euclidean. That should keep documents from differing just by dint of being different lengths. Though does TFIDF inherently normalize the vectors itself? Yes. TfidfVectorizer normalizes in a way such that cosine similarity is dot product when using norm='l2', which is the default. So never mind; cosine similarity SHOULD not change things, if cos similarity is always the same as Euclidean on normalized vectors. It's proportional to (1 - cos sim).
+# LSA is not an obvious win. Overall (LSA or not) it kinda groups by domain but actually tighter than that. Many clusters consist of 1 domain, but 1 domain is generally split across many clusters.
